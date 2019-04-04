9dcb29c08a102da34d94ecf64622b35cbfd8fe6b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 16:49:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function congratulateCustomer(){
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+    var favoriteCustomer = 'not bar'
 }
