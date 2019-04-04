4effed5cd02021950dc87db7514efa1b655a1241
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 16:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function overWriteBestCustomer(newFavorite){
 function congratulateCustomer(){
   return `congrats ${favoriteCustomer}`
 }
-
+  let favoriteCustomer = 'bar'
 function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
+  
     let favoriteCustomer = 'not bar'
 }
